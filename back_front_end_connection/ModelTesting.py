a9e96934b5b50b5223d13112b1c856ec1268964f
smokett <47299637+smokett@users.noreply.github.com>
2020-02-26 14:32:33 +0000

    Merged the ModelTesting and Parser with latest UI

--- back_front_end_connection/ModelTesting.py
+++ back_front_end_connection/ModelTesting.py
@@ -7,16 +7,15 @@ import tensorflow as tf
 
 class Testing:
 
-
 	def __init__(self,filepaths):
-		self.filepaths = list()
-		self.filepaths.append(filepaths)
+		self.filepaths = filepaths
+		print(filepaths)
 		self.i = 0
 
 	def analyse(self):
 		self.motion_keys = {0 : "HopLeft", 1 : "HopRight", 2 : "JogSpot", 3 : "JumpForward", 4 : "JumpHigh", 5 : "JumpSide"}
 		self.model = tensorflow.keras.models.load_model(r".\Current Model\current_model") #loaded model
-		self.output = open("output.txt","a")
+		self.output = open("output.txt","w")
 		for path in self.filepaths:
 
 				t_motion = pickle.load(open(path, "rb"))
@@ -33,37 +32,22 @@ class Testing:
 				
 				self.i+=1
 
-				if self.motion_keys[np.argmax(guesssed_motions)] == "HopLeft" and "HopLeft" in path:
+				if self.motion_keys[np.argmax(guesssed_motions)] == "HopLeft":
 					self.output.write("HopLeft\n")
-				elif self.motion_keys[np.argmax(guesssed_motions)] == "HopRight" and "HopRight" in path:
+				elif self.motion_keys[np.argmax(guesssed_motions)] == "HopRight":
 					self.output.write("HopRight\n")
-				elif self.motion_keys[np.argmax(guesssed_motions)] == "JogSpot" and "JogSpot" in path:
+				elif self.motion_keys[np.argmax(guesssed_motions)] == "JogSpot":
 					self.output.write("JogSpot\n")
-				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpForward" and "JumpForward" in path:
+				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpForward":
 					self.output.write("JumpForward\n")
-				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpHigh" and "JumpHigh" in path:
+				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpHigh":
 					self.output.write("JumpHigh\n")
-				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpSide" and "JumpSide" in path:
+				elif self.motion_keys[np.argmax(guesssed_motions)] == "JumpSide":
 					self.output.write("JumpSide\n")
 				else:
 					print("\nMotion", self.i)
 					print("Most Likely:", self.motion_keys[np.argmax(guesssed_motions)])
-					print("Mismatch on motion -", path)
-					self.output.write("Most Likely:", self.motion_keys[np.argmax(guesssed_motions)]+"Mismatch on motion -", path)
-					p = ""
-					if "HopLeft" in path:
-						p = "HopLeft"
-					elif "JogSpot" in path:
-						p = "JogSpot"
-					elif "HopRight" in path:
-						p = "HopRight"
-					elif "JumpForward" in path:
-						p = "JumpForward"
-					elif "JumpHigh" in path:
-						p = "JumpHigh"
-					elif "JumpSide" in path:
-						p = "JumpSide"
-					print("Actually :", p)
+					self.output.write("Most Likely:", self.motion_keys[np.argmax(guesssed_motions)])
 
 
 		self.output.close()
--- back_front_end_connection/gui.py
+++ back_front_end_connection/gui.py
@@ -1,115 +1,72 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton
-from PyQt5.QtCore import Qt, QUrl
-from back_front_end_connection.ModelTesting import Testing
+from mainFrame import Ui_MainWindow, links, linkNames
+import sys
+from fileReader import FileReader
+from ModelTesting import Testing
+from ParseData import Parser
 
+class MainWindowUIClass(Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.fileReader = FileReader()
+        
+    def setupUi(self, MW):
+        super().setupUi( MW )
 
-class ListBoxWidget(QListWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setAcceptDrops(True)
-        self.setDragDropOverwriteMode(True)
-        self.setGeometry(QtCore.QRect(30, 20, 301, 511))
+    def refreshAll(self):
+        selectedFileName = self.fileReader.getFileName()
+        print(selectedFileName)
+        links.append(selectedFileName)
+        self.lstbox.addItem(selectedFileName.split("/")[-1])
 
-    def dragEnterEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.accept()
-        else:
-            event.ignore()
+    def clearText(self):
+    	self.plainTextEdit.clear()
+    	self.plainTextEdit.setPlainText("Export CSV File Success")
 
-    def dragMoveEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.setDropAction(Qt.CopyAction)
-            event.accept()
-        else:
-            event.ignore()
+    # slot
+    def exportSlot(self):
+    	output = self.plainTextEdit.toPlainText()
+    	if output is not '':
+        	self.fileReader.writeDoc(output)
+        	self.clearText()
 
-    def dropEvent(self, event):
-        links = set([])
+    # slot
+    def importSlot(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
+                        None,
+                        "File Broswer",
+                        "",
+                        "All Files (*);;Python Files (*.py)",
+                        options=options)
+        if fileName:
+            self.fileReader.setFileName( fileName )
+            self.refreshAll()
 
-        if event.mimeData().hasUrls():
-            event.setDropAction(Qt.CopyAction)
-            event.accept()
-
-            for url in event.mimeData().urls():
-                if url.isLocalFile():
-                    links.add(str(url.toLocalFile()))
-                else:
-                    links.add(str(url.toString()))
-
-            self.addItems(links)
-            print(links)
-        else:
-            event.ignore()
-
-
-
-
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(948, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-
-        self.lstbox = ListBoxWidget(MainWindow)
-
-        self.readButton = QtWidgets.QPushButton(self.centralwidget)
-        self.readButton.setGeometry(QtCore.QRect(430, 250, 93, 28))
-        self.readButton.setObjectName("readButton")
-
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setGeometry(QtCore.QRect(620, 20, 301, 511))
-        self.plainTextEdit.setAcceptDrops(False)
-        self.plainTextEdit.setReadOnly(True)
-        self.plainTextEdit.setBackgroundVisible(False)
-        self.plainTextEdit.setObjectName("plainTextEdit")
-
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 948, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.readButton.clicked.connect(lambda: self.testFile())
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def getCurrentFile(self):
-        item = QListWidgetItem(self.lstbox.currentItem())
-        return item.text()
-
-    def testFile(self):
-        self.path = self.getCurrentFile()
-        print(self.path)
-        self.testing = Testing(str(self.path))
-        self.testing.analyse()
-        self.outputFile = open("output.txt","r")
-        self.text = self.outputFile.read() 
-        self.plainTextEdit.setPlainText(self.text)
-    
+    # slot
+    def readFileSlot(self):
+        self.path = QListWidgetItem(self.lstbox.currentItem()).text()
+        if self.path != '':
+            index = linkNames.index(self.path)
+            self.parseData = Parser(str(links[index]))
+            pickleName = self.parseData.parse()
+            self.testing = Testing(pickleName)
+            self.testing.analyse()
+            self.outputFile = open("output.txt","r")
+            self.text = self.outputFile.read() 
+            self.plainTextEdit.setPlainText(self.text)
 
 
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.readButton.setText(_translate("MainWindow", "Analyse"))
-        self.plainTextEdit.setDocumentTitle(_translate("MainWindow", "Output"))
-
-
-if __name__ == "__main__":
-    import sys
+def main():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = MainWindowUIClass()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    output = open("output.txt","w")
-    output.close()
     sys.exit(app.exec_())
+
+main()
