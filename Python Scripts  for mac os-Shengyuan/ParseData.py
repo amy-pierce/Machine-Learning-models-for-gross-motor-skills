48e44d49d126e40a853828e5c8d1fdff64f71d05
Liu shengyuan <lius8@tcd.ie>
2020-03-04 18:09:49 +0000

    try to update the mac verison,not finished

--- Python Scripts  for mac os-Shengyuan/ParseData.py
+++ Python Scripts  for mac os-Shengyuan/ParseData.py
@@ -4,93 +4,107 @@ import numpy as np
 import pickle
 import os
 import fnmatch
+import math
+from PyQt5.QtWidgets import QApplication
 
-FRAMES_SET = 1
+class Parser:
 
-files = list()
-for i,j,y in os.walk('Uncompressed'):
-	for n in y:
-		if fnmatch.fnmatch(n, '*.json'):
-			files.append(str(i))
+	def __init__(self,folder):
+		self.folder = folder
 
-for filename in files:
-	try:
-		#Whole data file
-		namestring = filename + "//skeleton.json"
-		data = json.load(open(namestring))
 
-		#Convert ground coordinates into an array of floats
-		ground_coordinates = data["g"]
-		ground_coordinates = re.findall(r"[-+]?\d*\.\d+|\d+", ground_coordinates)
-		for i, n in enumerate(ground_coordinates):
-			ground_coordinates[i] = float(n)
-
-		#get offset as a float
-		offset = float(data["o"])
-
-		ground_coordinates.append(offset)
-
-		#convert tracking data into a json file
-		unf = data["t"]
-		unf.replace("\\", "") #Remove backslashes from file to unescape ""
-		namestring = filename + "//track.json"
-		track = open(namestring, 'w')	#create new tracking data json document
-		track.write(unf)
-		items = json.load(open(namestring))
-
-		motion = list()
-		for frame in items['Items']: # for each individual frame of the skeleton.json file
-			#final frame data, appended to the motion
-			framedata = list()
-			framedata.append(ground_coordinates)
-			#includes list of whether the tracking is picking up
-			tracking = list()
-			for t in frame['b']['j']:
-				tracking.append(t['s'])
-
-			#includes list of position coordinates of each joint
-			j = 0
-			for position in frame['b']['j']:
-				posCoords = position['p']
-				posCoords = re.findall(r"[-+]?\d*\.\d+|\d+", posCoords)
-				for i, n in enumerate(posCoords):
-					posCoords[i] = float(n)
-				posCoords.append(0.0)
-				posCoords.append(float(tracking[j]))
-				j+=1
-				framedata.append(posCoords)
-
-			j = 0
-			#includes list of rotation offsets of each joint
-			for rotation in frame['b']['j']:
-				eulerVals = rotation['o']
-				eulerVals = re.findall(r"[-+]?\d*\.\d+|\d+", eulerVals)
-				for i, n in enumerate(eulerVals):
-					eulerVals[i] = float(n)
-				eulerVals.append(tracking[j])
-				j+=1
-				framedata.append(eulerVals)
-
-			#Add the full frame data to the motion
-			motion.append(framedata)
-
-		#Trims motion such that they are all divisible by 10
-		motion = motion[:(len(motion) - (len(motion) % FRAMES_SET))]
-		motion = [motion[i * FRAMES_SET:(i + 1) * FRAMES_SET] for i in range((len(motion) + FRAMES_SET - 1) // FRAMES_SET)]
-		motion = np.array(motion)
-
-		namestring = filename + "/tensor.pickle"
-		if len(motion) != 0:
-			motion = np.array(motion)
-			print("Writing to", namestring)
-			pickle_out = open(namestring, "wb")
-			pickle.dump(motion, pickle_out)
-			pickle_out.close()
+	def parse(self,progress):
+		files = list()
+		if(".json" in self.folder):
+			path = self.folder.split("")
+			path = path[:-1]
+			pathn = ""
+			for tmp in path:
+				pathn += tmp + ""
+			files.append(pathn)
 		else:
-			print("0 length file found :", namestring)
+			for i,j,y in os.walk(self.folder):
+				for n in y:
+					if fnmatch.fnmatch(n, '*.json'):
+						files.append(str(i))
 
-	except Exception as e:
-		print("\t\t\tv ERROR ON BELOW v")
-		print(e)
+		pickleName = list()
+		progress.pB.setValue(2)
+		pamount = len(files)
+		for filename in files:
+			# try:
+				#Whole data file
+				namestring = filename + "//skeleton.json"
+				tpath = namestring.replace("/",'\\')
+				progress.label.setText("Parsing " + tpath)
+				data = json.load(open(namestring))
 
-print("Compilaiton finished")
+				#Convert ground coordinates into an array of floats
+				ground_coordinates = data["g"]
+				ground_coordinates = re.findall(r"[-+]?\d*\.\d+|\d+", ground_coordinates)
+				for i, n in enumerate(ground_coordinates):
+					ground_coordinates[i] = float(n)
+
+				#get offset as a float
+				offset = float(data["o"])
+
+				ground_coordinates.append(offset)
+
+				#convert tracking data into a json file
+				unf = data["t"]
+				unf.replace("\\", "") #Remove backslashes from file to unescape ""
+				namestring = filename + "//track.json"
+				track = open(namestring, 'w')	#create new tracking data json document
+				track.write(unf)
+				items = json.load(open(namestring))
+
+				motion = list()
+				for frame in items['Items']: # for each individual frame of the skeleton.json file
+					#final frame data, appended to the motion
+					framedata = list()
+					framedata.append(ground_coordinates)
+					#includes list of whether the tracking is picking up
+					tracking = list()
+					for t in frame['b']['j']:
+						tracking.append(t['s'])
+
+					#includes list of position coordinates of each joint
+					j = 0
+					for position in frame['b']['j']:
+						posCoords = position['p']
+						posCoords = re.findall(r"[-+]?\d*\.\d+|\d+", posCoords)
+						for i, n in enumerate(posCoords):
+							posCoords[i] = float(n)
+						posCoords.append(0.0)
+						posCoords.append(float(tracking[j]))
+						j+=1
+						framedata.append(posCoords)
+
+
+					#Add the full frame data to the motion
+					motion.append(framedata)
+
+
+				namestring = filename + "/tensor.pickle"
+				if namestring not in pickleName:
+					pickleName.append(namestring)
+				if len(motion) != 0:
+					motion = np.array(motion)
+					print("Writing to", namestring)
+					progress.label.setText("Writing to " + namestring)
+					pickle_out = open(namestring, "wb")
+					pickle.dump(motion, pickle_out)
+					pickle_out.close()
+				else:
+					print("0 length file found :", namestring)
+				QApplication.processEvents()
+				progress.pB.setValue(progress.pB.value() + math.floor((1/pamount) * 20))
+				QApplication.processEvents()
+
+			# except Exception as e:
+			# 	print("\t\t\tv ERROR ON BELOW v")
+			# 	print(e)
+
+		progress.pB.setValue(20)
+		print("Compilaiton finished")
+		return pickleName
---
+++ Python Scripts  for mac os-Shengyuan/gui.py
@@ -0,0 +1,104 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QFileDialog, QWidget, \
+    QVBoxLayout
+from mainFrame import Ui_MainWindow, links, linkNames
+import sys
+import time
+from fileReader import FileReader
+from ModelTesting import Testing
+from ParseData import Parser
+
+
+class MainWindowUIClass(Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.fileReader = FileReader()
+
+    def setupUi(self, MW):
+        super().setupUi(MW)
+
+    def refreshAll(self):
+        selectedFileName = self.fileReader.getFileName()
+        links.append(selectedFileName)
+        self.lstbox.index += 1
+        onlyFileName = selectedFileName.split("/")[-1]
+        linkNames.append(str(self.lstbox.index) + ". " + onlyFileName)
+        self.lstbox.addItem(str(self.lstbox.index) + ". " + onlyFileName)
+
+    def clearText(self):
+        self.plainTextEdit.clear()
+        self.plainTextEdit.setPlainText("Export CSV File Success")
+
+    # slot
+    def exportSlot(self):
+        output = self.plainTextEdit.toPlainText()
+        if output is not '':
+            self.fileReader.writeDoc(output)
+            self.clearText()
+
+    # slot
+    def importFileSlot(self):
+        options = QtWidgets.QFileDialog.Options()
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
+            None,
+            "File Broswer",
+            "",
+            "All Files (*);;Python Files (*.py)",
+            options=options)
+        if fileName:
+            self.fileReader.setFileName(fileName)
+            self.refreshAll()
+
+    # slot
+    def importDirectorySlot(self):
+        getExistingDirectory = QFileDialog.getExistingDirectory
+        fileName = getExistingDirectory(None,
+                                        'Folder Broswer',
+                                        "")
+        if fileName:
+            self.fileReader.setFileName(fileName)
+            self.refreshAll()
+
+    # slot
+    def readFileSlot(self):
+        self.path = QListWidgetItem(self.lstbox.currentItem()).text()
+        self.showProgress = PopUpProgressBar()
+        self.showProgress.show()
+        if self.path != '':
+            index = linkNames.index(self.path)
+            self.parseData = Parser(str(links[index]))
+            pickleName = self.parseData.parse(self.showProgress)
+            self.testing = Testing(pickleName)
+            self.results = self.testing.analyse(self.showProgress)
+            self.text = ""
+            for result in self.results:
+                self.text += result[0] + " is " + result[1] + "% " + result[2] + "\n"
+            self.plainTextEdit.setPlainText(self.text)
+
+
+class PopUpProgressBar(QWidget):
+    def __init__(self, parent=None):
+        super(PopUpProgressBar, self).__init__(parent)
+        self.box = QtWidgets.QVBoxLayout(self)
+        self.pB = QtWidgets.QProgressBar(self)
+        self.label = QtWidgets.QLabel("Hi", self)
+        self.label.setGeometry(12, 0, 700, 50)
+        self.pB.setRange(0, 100)
+        self.pB.setGeometry(30, 40, 700, 100)
+        self.box.addWidget(self.pB)
+        self.setLayout(self.box)
+        self.setGeometry(300, 300, 550, 100)
+        self.setWindowTitle('Progress')
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = MainWindowUIClass()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+main()
